ca8c35829cdccb3def4ab4dc8ef681a512f56554
Varni Rachupally <varnirachupally@macbook-pro.lan>
2022-12-07 19:52:06 -0500

    Added id to customerbusiness

--- models/Customerbusiness.js
+++ models/Customerbusiness.js
@@ -5,6 +5,13 @@ class Customer_business extends Model {}
 
 Customer_business.init(
   {
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
